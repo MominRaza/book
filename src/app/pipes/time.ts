c5c02ffe6c625ad70a0d663e3624fafaf56581db
Momin Ahmad <mominr082@gmail.com>
2026-01-11 07:54:41 +0530

    feat: enhance time pipe; improve time formatting to include hours and ensure proper zero-padding for minutes and seconds

--- src/app/pipes/time.ts
+++ src/app/pipes/time.ts
@@ -5,8 +5,18 @@ import { Pipe } from "@angular/core";
 })
 export class Time {
   transform(value: number): string {
-    const minutes = Math.floor(value / 60);
+    const hours = Math.floor(value / 3600);
+    const minutes = Math.floor((value % 3600) / 60);
     const seconds = Math.floor(value % 60);
-    return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+
+    const parts = [];
+    if (hours > 0) {
+      parts.push(hours.toString());
+      parts.push(minutes.toString().padStart(2, "0"));
+    } else {
+      parts.push(minutes.toString());
+    }
+    parts.push(seconds.toString().padStart(2, "0"));
+    return parts.join(":");
   }
 }
